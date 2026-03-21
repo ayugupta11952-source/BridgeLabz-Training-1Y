e4ff4feca32383d26ad71f01ef1fa6ff631e9ed7
Ayush Gupta <ayugupta11952@gmail.com>
2026-03-21 22:38:49 +0530

    Added solns

---
+++ Methods/src/com/gla/methods/level1/Trigonometry.java
@@ -0,0 +1,19 @@
+package com.gla.methods.level1;
+import java.util.*;
+public class Trigonometry {
+    public static double[] calculateTrigonometricFunctions(double angle){
+        double rad=Math.toRadians(angle);
+        double sin=Math.sin(rad);
+        double cos=Math.cos(rad);
+        double tan=Math.tan(rad);
+        return new double[]{sin,cos,tan};
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        double angle=sc.nextDouble();
+        double[] res=calculateTrigonometricFunctions(angle);
+        System.out.println("Sin: "+res[0]);
+        System.out.println("Cos: "+res[1]);
+        System.out.println("Tan: "+res[2]);
+    }
+}
